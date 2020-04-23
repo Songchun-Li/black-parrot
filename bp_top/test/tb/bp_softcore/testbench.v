6ad782769f84f0cbf0372a5e19ab1334394577a4
Dan Petrisko <petrisko@cs.washington.edu>
2020-04-23 16:24:43 -0700

    Removing npc check from scheduler

--- bp_top/test/tb/bp_softcore/testbench.v
+++ bp_top/test/tb/bp_softcore/testbench.v
@@ -573,7 +573,7 @@ bind bp_be_top
 
        ,.fe_cmd(fe.pc_gen.fe_cmd_yumi_o & ~fe.pc_gen.attaboy_v)
 
-       ,.mispredict(be.be_checker.scheduler.npc_mismatch)
+       ,.mispredict(be.be_checker.director.npc_mismatch_v)
        ,.target(be.be_checker.director.isd_status.isd_pc)
 
        ,.dtlb_miss(be.be_mem.dtlb_miss_r)
